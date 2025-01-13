64d63645949d3e35e6d100b036737db0c3376966
dbenfon341 <dbenfon341@g.educaand.es>
2025-01-13 21:26:55 +0100

    Ejercicio 2 semidone.
    + TODO: Crear función para pedir nombre.
    + Editar estructura de prints.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,38 @@
+import classes.Persona
+
 fun main() {
-    println("Hello World!")
+
+    val persona1 = Persona(10.0, 1.70)
+    val persona2 = Persona(50.0, 1.60, "Manuel")
+    val persona3 = Persona(65.0,1.20, "Pepe")
+
+
+    println("\n*********************************************")
+    println(persona1)
+    println("*********************************************\n")
+    println(persona2)
+    println("*********************************************\n")
+    println(persona3)
+    println("*********************************************\n")
+
+
+    // TODO Crear función.
+    print("Introduce nombre para persona1 >> ")
+    val nombre = readln()
+    persona1.nombre = nombre
+    println("Persona1 actualizado \n*********************************************")
+    println("Nombre: ${persona1.nombre}, Peso: ${persona1.peso}, Altura: ${persona1.altura}")
+
+
+    println("\n*********************************************\n")
+    println("Peso: ${persona3.peso}, Altura: ${persona3.altura}, IMC: ${persona3.calcularImc()}")
+    persona3.altura = 1.80
+    println("Peso: ${persona3.peso}, Altura: ${persona3.altura}, IMC: ${persona3.calcularImc()}")
+
+    if (persona3.equals(persona1)) {
+        println("Es igual")
+    } else {
+        println("No es igual")
+    }
+
 }
--- src/classes/Persona.kt
+++ src/classes/Persona.kt
@@ -1,4 +1,31 @@
 package classes
 
-class Persona(peso: Double, var altura: Double) {
+class Persona(var peso: Double, var altura: Double) {
+    // Constructor primario.
+
+
+    // Constructor secundario.
+    var nombre = "NONAME"
+    constructor(peso: Double, altura: Double, nombre: String) : this(peso, altura) {
+        this.nombre = nombre
+    }
+
+    // Función que calcula IMC.
+    fun calcularImc(): Double {
+        return peso / (altura * altura)
+    }
+
+
+    // toString para mostrar la información de la persona en cuestión.
+    override fun toString(): String {
+        return "nombre: $nombre, peso: $peso, altura: $altura, imc: ${"%2f".format(calcularImc())}"
+    }
+
+    // Equals para verificar si dos objetos son iguales o no.
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Persona) return false
+
+        return this.peso == other.peso && this.altura == other.altura
+    }
 }
